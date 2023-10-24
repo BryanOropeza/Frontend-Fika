b9c1dac016e8a5f174246b40a550a437059b2381
BryanOropeza <bryan.oropeza16@gmail.com>
2023-10-24 06:39:13 -0500

    Primer Commit Frontend - Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { CitaService } from 'src/services/cita.service';
+import { PacienteService } from 'src/services/paciente.service';
+import { UsuarioService } from 'src/services/usuario.service';
+import { RolService } from 'src/services/rol.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend_fika';
+
+  constructor(
+    public fb: FormBuilder,
+
+  ) {
+
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
